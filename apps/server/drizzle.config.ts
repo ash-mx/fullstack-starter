17b6fa835b14be72bcdcfb09be0829fbb9414a26
ash-mx <ari@aripxl.com>
2025-08-07 10:09:12 +1000

    Added Drizzle

---
+++ apps/server/drizzle.config.ts
@@ -0,0 +1,15 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+import { CONFIG_PATH } from "./src/lib/lib.config";
+
+config({ path: CONFIG_PATH });
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/db/schema/schema.ts",
+  dialect: "postgresql",
+  casing: "snake_case",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
---
+++ apps/server/src/db/index.ts
@@ -0,0 +1,17 @@
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema/schema";
+import { CONFIG_PATH } from "../lib/lib.config";
+
+config({ path: CONFIG_PATH });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle({
+  client: pool,
+  schema: schema,
+  casing: "snake_case",
+});
